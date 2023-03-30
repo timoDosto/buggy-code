6dda77e90e68d110d5e10bdc480214f1f8c8471f
unknown <xdostal5@ad.fi.muni.cz>
2023-03-30 12:55:14 +0200

    dsds

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,3 @@
-import sys 
-import os
 import yaml
 import flask
 
@@ -37,13 +35,15 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
 def authenticate(password):
     # Assert that the password is correct
-    assert password == "Iloveyou", "Invalid password!"
-    print("Successfully authenticated!")
+    if password != "Iloveyou":
+        print("Invalid password!")
+    else:
+        print("Successfully authenticated!")
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
